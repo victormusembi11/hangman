6721c7e4d06ac8b9c4a0335d212c519cf1a36838
victormusembi11 <victor.musembi11@gmail.com>
2023-11-23 16:42:57 +0300

    hangman word component

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,3 +1,36 @@
 export function HangmanWord() {
-  return <div>Hello</div>;
+  const word = "hello";
+  const guessedLetters = ["h", "g", "l"];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25rem",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => (
+        <span
+          style={{
+            borderBottom: ".1em solid black",
+          }}
+          key={index}
+        >
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 }

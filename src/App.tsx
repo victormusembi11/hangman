3656ab6ea9d8aa12bac124977aa905981058e34b
victormusembi11 <victor.musembi11@gmail.com>
2023-11-23 16:12:46 +0300

    initialised base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
 function App() {
-  return <>Hello World</>;
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          fontSize: "2rem",
+          textAlign: "center",
+        }}
+      >
+        Lose Win
+      </div>
+    </div>
+  );
 }
 
 export default App;

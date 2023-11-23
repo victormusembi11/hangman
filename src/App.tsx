40ddc0ee28b7c15a5c45a628167767ab097eef39
victormusembi11 <victor.musembi11@gmail.com>
2023-11-23 17:11:04 +0300

    drawing component logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
 import { HangmanDrawing } from "./components/HangmanDrawing";
 import { HangmanWord } from "./components/HangmanWord";
 import { Keyboard } from "./components/Keyboard";
 
+import words from "./wordlist.json";
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  // all the incorrect letters are the ones that are not included in the word to guess
+  const incorrectLetters = guessedLetters.filter((letter) => {
+    !wordToGuess.includes(letter);
+  });
+
   return (
     <div
       style={{
@@ -22,7 +36,7 @@ function App() {
       >
         Lose Win
       </div>
-      <HangmanDrawing />
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
       <HangmanWord />
       <div style={{ alignSelf: "stretch" }}>
         <Keyboard />
